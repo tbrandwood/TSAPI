1ce56cb4abdf39b6b32c025e5030c2f97b589b87
Tim Brandwood <tim@digitaltaxonomy.co.uk>
2020-11-15 16:04:42 +0000

    Implementing Demo controller - WIP

---
+++ Domain/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Enums
+    {
+        public enum ResultCode
+        {
+            Ok = 0
+        }
+
+    }
+}
---
+++ TSAPI/Controllers/ExtensionMethods/HttpExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TSAPI.Controllers.ExtensionMethods
+{
+    public static class HttpExtensions
+    {
+
+        public static string GetApiKey(this HttpRequest request)
+        {
+            if (!request.Headers.ContainsKey("Authorisation"))
+                throw new SecurityException("Autorisation header not found in request");
+                
+            return request.Headers["Authorisation"];
+        }
+    }
+}
